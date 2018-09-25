86719c607ffe00f9a0c5baf8d1755262b4264cd6
jonivde <jonivde@hotmail.com>
2018-09-25 16:17:49 +0200

    Update LSTM.py

--- LSTM.py
+++ LSTM.py
@@ -8,7 +8,7 @@ from keras.preprocessing.sequence import pad_sequences
 #Y_train (TODO: convert strings to ints)
 speech_acts = [0, 1, 2]
 #X_train
-speech_utterances = ['hallo dit is',
+speech_utterances= ['hallo dit is',
                      'ook een regel',
                      'hallo en nog een regel hallo hallo hallo i j k l m']
 
@@ -19,23 +19,24 @@ vocab_size = len(tokenizer.word_index)  #store vocabulary size for model input
 sequences = tokenizer.texts_to_sequences(speech_utterances) #convert words to vocabulary integers matrix
 
 #apply padding
-padded_text = pad_sequences(sequences, padding='post', maxlen=10)
-print(vocab_size)
+padded_text = pad_sequences(sequences, padding='post', maxlen=10, truncating='post')
+print(tokenizer.word_index)
 print(padded_text)
 
 #create LSTM
 model = Sequential()
-model.add(Embedding(input_dim=vocab_size+1, mask_zero=True, output_dim=100))
-model.add(LSTM(100, activation='relu'))
+model.add(Embedding(input_dim=vocab_size+1, mask_zero=True, output_dim=100, input_length=10)) #outputs 3D tensor
+model.add(LSTM(50, activation='relu'))
 model.add(Dense(units=15, activation='softmax'))
 
 #train model
 model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
-              metrics=['accuracy'])
+              metrics='accuracy')
 
-#fit model
-score = model.fit(padded_text, speech_acts, epochs=2)
+model.fit(padded_text, speech_acts, epochs=2)
 
-#print(score)
+#test model
+score = model.evaluate()
+print(score)
 #train model compile() >  categorical cross-entropy as loss function and accuracy as evaluation measure.
