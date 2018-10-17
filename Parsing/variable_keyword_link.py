b4c2f82a5782be2dde812e4e482fba08b3e42925
jonivde <jonivde@hotmail.com>
2018-10-17 15:03:45 +0200

    Update variable_keyword_link.py

--- Parsing/variable_keyword_link.py
+++ Parsing/variable_keyword_link.py
@@ -7,22 +7,45 @@ variable_keyword_link = {
     "name": []
 }
 
-def read_json(file):
-    with open(file) as d:
-        ontology_dstc2 = json.load(d)
-        informable_dict = ontology_dstc2["informable"]
-    print(informable_dict)
+#TODO define parsed sentence full
+#TODO define disjointness
+parsed_sentence = ["i want chinese food", "i", "want chinese food", "want", "chinese food", "chinese", "food", "want chinese food in the south part of town", "in the south part of town", "in", "the south part of town", "the", "south part of town", "south part", "of town", "south", "town"]
+parsed_sentence_full = "i want chinese food in the south part of town"
+
+#specify path
+with open('C:/Users/joniv/Documents/MAIR_lab/chatbot project/results/ontology_dstc2.json') as d:
+    ontology_dstc2 = json.load(d)
+    informable_dict = ontology_dstc2["informable"]
+
+
 
 #select smallest parsed_sentence with type s as input
 def user_preference_identifier(parsed_sentence):
     #list with slot values = empty
-    #per value_slot search if trigger word is present
-        #if not return NULL
-        #else
-            #look at parsing tree sentence and search for leafs with trigger word
-                #if found > search for accompanying leafs with keyword
-                    #put all nodes in list (double nodes excluded)
-                    #make value equal to last of nodelist
-    #check if nodelists for different values are disjoint
-    if __name__ == "__main__":
-        read_json('C:/Users/joniv/Documents/MAIR_lab/chatbot project/results/ontology_dstc2.json')
+
+    found_word_list = []
+
+    for key, value in informable_dict.items():
+        print(key)
+        found_word_list.clear()
+        for trigger_word in value:
+            if trigger_word in parsed_sentence_full:
+                found_word_list.append(trigger_word)
+                for link_word in variable_keyword_link[key]:
+                    if link_word in parsed_sentence_full:
+                        found_word_list.append(link_word)
+                        print(str(found_word_list) + " dit is de link + trigger word list die ie gevonden heeft")
+    substring_finder(found_word_list, parsed_sentence)
+                           
+
+def substring_finder(found_word_list, parsed_sentence):
+    substring_list = []
+    for substring in parsed_sentence:
+        if all(word in substring for word in found_word_list):
+            substring_list.append(substring)
+    shortest_substring = min(substring_list, key=len)
+    print(shortest_substring + " ANTWOORD")
+    return shortest_substring
+
+if __name__ == "__main__":
+    user_preference_identifier(parsed_sentence)
