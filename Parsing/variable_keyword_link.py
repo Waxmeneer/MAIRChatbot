599079438ab3f0625ea94ed2f232966a5bfc8217
jonivde <jonivde@hotmail.com>
2018-10-17 13:26:38 +0200

    Update variable_keyword_link.py

--- Parsing/variable_keyword_link.py
+++ Parsing/variable_keyword_link.py
@@ -1,17 +1,17 @@
+import json
+
 variable_keyword_link = {
-    "addr": [],
     "area": ["town", "part"],
     "food": ["restaurant", "food"],
-    "phone": [],
     "pricerange": ["restaurant", "priced"],
-    "postcode": [],
-    "signature": [],
     "name": []
 }
 
-#import .json ontology
-
-
+def read_json(file):
+    with open(file) as d:
+        ontology_dstc2 = json.load(d)
+        informable_dict = ontology_dstc2["informable"]
+    print(informable_dict)
 
 #select smallest parsed_sentence with type s as input
 def user_preference_identifier(parsed_sentence):
@@ -24,3 +24,5 @@ def user_preference_identifier(parsed_sentence):
                     #put all nodes in list (double nodes excluded)
                     #make value equal to last of nodelist
     #check if nodelists for different values are disjoint
+    if __name__ == "__main__":
+        read_json('C:/Users/joniv/Documents/MAIR_lab/chatbot project/results/ontology_dstc2.json')
