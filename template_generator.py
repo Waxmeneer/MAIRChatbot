e4b3f46bf9939a7506fb31d9643def9bdeb8df8e
jonivde <jonivde@hotmail.com>
2018-10-25 15:29:28 +0200

    Update template_generator.py

--- template_generator.py
+++ template_generator.py
@@ -1,34 +1,58 @@
+import csv
 
-def restaurant_finder(filled_slots):
-    #if not all slots are filled
-        #n of restaurants + info of slots + additional question about empty slot
-        #restaurant
-    #if all slots filled
-        #restaurant = name
-        #system_utterance = print(restaurant) + info of slots
-
-    return system_utterance, restaurant
-
-def info_finder(restaurant, request):
-    #request_value = find request header in . csv in row of restaurant
-    system_utterance = "The" + print(request) + "of"+ print(restaurant) + "is" + print(request_value)
-    return system_utterance
-
-def template_generator(speech_act, sentence):
-    filled_slots = []
-    restaurant = ""
-    system_utterance = []
+#Keep track of suggested restaurant if user wants another restaurant
+#Make templates and keep track of order in which user states preferences
+#aggregation?
 
 
-    #if speech act = inform()
-        #update filled slots
-            #if no value provided > update with random
-        #system_utterance = restaurant_finder(filled_slots)
-        # if system utterance starts with name
-            #update restaurant
-    #if speech act = request()
-        #what is request?
-        #system_utterance = info_finder(restaurant)
+#makes a list with lists of possible restaurants
+#typical element in restaurant list is [name, pricerange, area, food, phone, addr, postcode]
+def csv_reader():
+    with open('C:/Users/joniv/Documents/AI_courses/MAIR/MAIR_lab/MAIRChatbot-master/restaurantinfo.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        restaurant_info = []
+        for row in csv_reader:
+            restaurant_info.append(list(row))
+        restaurant_info.pop(0)
+    return restaurant_info
+
+def restaurant_finder(filled_slots, restaurant_info):
+    possible_restaurants = []
+    values_filled_slots = []
+    
+    #make list of slot values which are not None
+    for key, value in filled_slots.items():
+            values_filled_slots.append(str(value))
+
+    #make list of restaurant values and compare
+    for restaurant in restaurant_info:
+        info_elements = restaurant[1:4]
+        if set(values_filled_slots).issubset(info_elements):
+            print("YES")
+            possible_restaurants.append(restaurant)
 
 
-    return system_utterance
+    if len(possible_restaurants) == 1:
+        return str(restaurant[0]) + " is a nice place " + str(template_generator(filled_slots, 1))
+    else:
+        return "there are " + str(len(possible_restaurants)) + " restaurants" + str(template_generator(filled_slots, 0))
+
+
+#gets if final restaurant or not (enkelvoud of meervoud zin teruggeven)
+def template_generator(filled_slots, final_restaurant):
+    #TODO make templates
+    return " hello"
+
+if __name__ == "__main__":
+    #need to be filled in volgorde van de user als er geen slot is niks meegeven
+    filled_slots = {
+        "pricerange": "expensive",
+        "area": "north",
+        "food": "french"
+    }
+    restaurant_info = csv_reader()
+    print(restaurant_finder(filled_slots, restaurant_info))
+
+
+
+
