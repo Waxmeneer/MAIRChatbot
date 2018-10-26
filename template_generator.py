19e09e12e447e9f72a5632426b11c3141ed58e5b
jonivde <jonivde@hotmail.com>
2018-10-26 12:47:40 +0200

    Update template_generator.py

--- template_generator.py
+++ template_generator.py
@@ -1,5 +1,5 @@
 import csv
-
+from LSTM import model_trainer, model_user
 #Keep track of suggested restaurant if user wants another restaurant
 #Make templates and keep track of order in which user states preferences
 #aggregation?
@@ -16,10 +16,27 @@ def csv_reader():
         restaurant_info.pop(0)
     return restaurant_info
 
+def speech_act_finder(sentence):
+    modelinf = model_trainer()
+    model = modelinf[0]
+    tokenizer = modelinf[1]
+    speech_act = model_user(model, tokenizer, sentence)
+    return speech_act
+
+def request_info_finder(restaurant, info):
+    if info == "phone":
+        return "the phone number is" + str(restaurant[4])
+    if info == "addr":
+        return "the address is " + str(restaurant[5])
+    if info == "postcode":
+        return "the postcode is" + str(restaurant[6])
+    return
+
 def restaurant_finder(filled_slots, restaurant_info):
     possible_restaurants = []
     values_filled_slots = []
 
+    #make list of slot values which are not None
     for key, value in filled_slots.items():
             values_filled_slots.append(str(value))
 
@@ -27,15 +44,8 @@ def restaurant_finder(filled_slots, restaurant_info):
     for restaurant in restaurant_info:
         info_elements = restaurant[1:4]
         if set(values_filled_slots).issubset(info_elements):
-            print("found restaurant")
             possible_restaurants.append(restaurant)
-
-
-    if len(possible_restaurants) == 1:
-        return str(restaurant[0]) + " is a nice place " + str(template_generator(filled_slots, 1))
-    else:
-        return "there are " + str(len(possible_restaurants)) + " restaurants" + str(template_generator(filled_slots, 0))
-
+    return possible_restaurants
 
 #gets if final restaurant or not (enkelvoud of meervoud zin teruggeven)
 def template_generator(filled_slots, final_restaurant):
@@ -51,6 +61,8 @@ if __name__ == "__main__":
     }
     restaurant_info = csv_reader()
     print(restaurant_finder(filled_slots, restaurant_info))
+    #analyze speech act for every sentence
+    #keep track of current restaurant
 
 
 
