031bf091f4699320a2941aeb8db0b8152e5241ef
mauwie1 <m.r.politiek@students.uu.nl>
2018-10-26 13:33:24 +0200

    Add files via upload

--- Parsing/variable_keyword_link.py
+++ Parsing/variable_keyword_link.py
@@ -21,24 +21,36 @@ def user_preference_identifier(parsed_sent, sent):
     slot_list = []
     node_list = []
     found_word_list = []
-    for key, value in informable_dict.items():
-        found_word_list.clear()
-        for trigger_word in value:
-            if trigger_word in sent:
-                found_word_list.append(trigger_word)
+    splitsent = sent.split()
+    for word in splitsent:
+        for key, value in informable_dict.items():
+            found_word_list.clear()
+            if word in value:
+                found_word_list.append(word)
+                templist = []
                 for link_word in variable_keyword_link[key]:
-                    if link_word in sent:
-                        found_word_list.append(link_word)
+                    if link_word in splitsent:
+                        templist.append(link_word)
+                found_word_list+=templist
                 slot_list.append([key,topnode_finder(found_word_list, parsed_sent)])
-
-    slot_list = duplicate_remover(slot_list)                #this function removes all duplicates from the list, this sometimes happens if multiple linkedwords are present in a sentence
     for slot in slot_list:
-        node_list.append(subtree_finder(slot[1], parsed_sent, []))
-    if(disjoint_checker(node_list)): #This function checks if the trees are disjoint or not
-        return None
+        index=-1
+        for parse in parsed_sent:
+            if parse[0]==slot[1]:
+                index=parse[4]
+        node_list.append(subtree_finder(slot[1], parsed_sent, [], index))
+    return [slot_list, node_list]
+
+#Prints whether trees in the node_list are disjoint.
+def disjoint_printer(node_list):
+    for node in node_list:
+        for item in node:
+            print(item[0])
+        print("")
+    if disjoint_checker(node_list):
+        print('Disjoint')
     else:
-        return slot_list
-        
+        print('Not disjoint')
 
 #this function finds the top node of the subtree that contains both the triggerword and all linkedwords
 def topnode_finder(found_word_list, parsed_sentence):
@@ -51,15 +63,15 @@ def topnode_finder(found_word_list, parsed_sentence):
     smallest =  min(subtree_list, key=len)
     for node in parsed_sentence:
         if node[0][1] == smallest:
-            return node[0] #Returns the top node of the smallest substring.
+            return node[0] #Returns the top node of the smallest substring, with its index.
 
 #This function checks if two sets of nodes are disjoint.
-def disjoint_subtrees(nodes1, nodes2):
+def joint_subtrees(nodes1, nodes2):
     disjoint = False
     for node in nodes1:
         if node in nodes2:
-            disjoint = True
-    return disjoint
+            joint = True
+    return joint
 
 #Checks if any of the trees in a list are disjoint. If so, it returns True, else False.
 def disjoint_checker(treelist):
@@ -69,37 +81,40 @@ def disjoint_checker(treelist):
         c2=c+1
         while c2<len(treelist):
             tree2 = treelist[c2]
-            if(disjoint_subtrees(tree, tree2)):
-               return True
+            if(joint_subtrees(tree, tree2)):
+               return False
             c2+=1
         c+=1
-    return False
+    return True
 
 #Finds the subtree belonging to a top node.
-def subtree_finder(topnode, parsed_sent, nodelist):
-    nodelist.append(topnode)
+def subtree_finder(topnode, parsed_sent, nodelist, index):
+    nodelist.append([topnode, index])
     for node in parsed_sent:
         if node[0]==topnode:
             subtr1 = node[1]
             subtr2 = node[2]
-            subtree_finder(subtr1, parsed_sent, nodelist)
-            subtree_finder(subtr2, parsed_sent, nodelist)
+            index = node[4]
+            subtree_finder(subtr1, parsed_sent, nodelist, index)
+            subtree_finder(subtr2, parsed_sent, nodelist, index)
             return(nodelist)
 
+#labels the nodes, so two nodes that are the same but at different places in the sentence are able to be distinguished.
+def label_nodes(parsed_sent):
+    c=0
+    for parse in parsed_sent:
+        parse.append(c)
+        c+=1
+    return parsed_sent
 
-
-#this function removes duplicates from any lists that is used as input
-def duplicate_remover(list):
-    no_duplicates_list = []
-    for slot in list:
-        if slot in no_duplicates_list:
-            pass
-        else:
-            no_duplicates_list.append(slot)
-    return no_duplicates_list
 
 if __name__ == "__main__":
     inp= input("Find user preference. Sentence? ")
     parsed_sentence = wordparsesteps(inp)
-    print(user_preference_identifier(parsed_sentence, inp))
+    parsed_sentence = label_nodes(parsed_sentence)
+    user_pref = user_preference_identifier(parsed_sentence, inp)
+    disjoint_printer(user_pref[1])
+    slot_list = user_pref[0]
+    if disjoint_checker(user_pref[1]):
+        pass###
 
