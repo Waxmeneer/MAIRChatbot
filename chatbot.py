61647f76e70994980bbade32d105252ae1076f75
jonivde <jonivde@hotmail.com>
2018-10-26 14:51:24 +0200

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,13 @@
-def manager():
-    user_sentences = []
-    suggested_restaurant = []
-    slot_list = []
 
+user_sentences = []
+suggested_restaurant = []
+slot_list = []
+
+def dialogue_ender():
+    #clear all global variables
+
+def manager():
+    
     #start dialogue
     #welcome message
         #read user input + store in user_sentences + store speechact as list
@@ -14,7 +19,7 @@ def manager():
             #if 1 such restaurant exists: return restaurant
                 #store suggested restaurant
             #if more: return multiple restaurants and ask for missing slot info
-            #if no restaurant exists: give back to user and ask other preferences 
+            #if no restaurant exists: give back to user and ask other preferences
 
 
     #chat
@@ -23,3 +28,4 @@ def manager():
 def sentence_checker(sentence):
     #store user sentence
     #determine speech act
+
