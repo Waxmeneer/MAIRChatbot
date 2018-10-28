b2bd845955c7843eea7fc45408966f2571e9ab44
al <a.k.sugiharto@gmail.com>
2018-10-28 05:26:41 +0100

    update chatbot and template generator

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 from template_generator import *
 
-user_sentences = {}
-current_suggested_restaurant = {}
+user_sentences = []
+current_suggested_restaurant = []
 filled_slots = {}
 
 def dialogue_ender():
@@ -10,8 +10,8 @@ def dialogue_ender():
     global current_suggested_restaurant
     global filled_slots
     filled_slots = {}
-    user_sentences = {}
-    current_suggested_restaurant = {}
+    user_sentences = []
+    current_suggested_restaurant = []
 
 def manager():
     global user_sentences
@@ -23,6 +23,7 @@ def manager():
 
     # start dialogue
     while True:
+        # GET SPEECH ACT
         # to check if this is just welcome
         if is_welcome:
             speech_act = 'hello'
@@ -30,40 +31,44 @@ def manager():
         else:
             speech_act = speech_act_finder(user_response)
 
+        # STORE SPEECH ACT AND SENTENCE
+        user_sentence = {}
+        user_sentence['sentence'] = user_response
+        user_sentence['speech_act'] = speech_act
+        user_sentences.append(user_sentence)
+
         # get filled slots, only if the speech act is inform
         if speech_act == 'inform':
-            #TODO fill slots real
-            filled_slots = {
+            #TODO get real slots value
+            get_filled_slots = {
                 "pricerange": "expensive",
                 "area": "north",
                 "food": "french"
             }
+            # STORE SLOTS VALUE
+            filled_slots = get_filled_slots
 
-        # read user input
-        template_result = template_generator(filled_slots, restaurant_info, speech_act, current_suggested_restaurant)
-        #+ store in user_sentences + store speechact as list
+            #GET POSSIBLE RESTAURANT
+            current_suggested_restaurant = restaurant_finder(filled_slots, restaurant_info)
+
+        # GET TEMPLATE
+        template_result = template_generator(filled_slots, speech_act, current_suggested_restaurant)
 
         print('System: '+template_result)
+
+        # if no restaurant found, restart the dialogue
+        if template_result != "We cannot find any restaurant. What other kind of restaurant you like?":
+            dialogue_ender()
 
         # as long as the template result is not the end of dialogue, then keep expecting user input
         if template_result != 'Thank you for using team14 system. Bye.':
             user_response = input('User: ')
         # ending the dialog by saying bye and exiting the system
         else:
-            # end dialogue
+            # END DIALOGUE
             break
 
         is_welcome = False;
-
-
-    #else:
-        #store filled slots in user sentences
-        #check if such restaurant with preferences exist
-            #if 1 such restaurant exists: return restaurant
-                #store suggested restaurant
-            #if more: return multiple restaurants and ask for missing slot info
-            #if no restaurant exists: give back to user and ask other preferences
-
 
 #def sentence_checker(sentence):
     #store user sentence
--- template_generator.py
+++ template_generator.py
@@ -55,30 +55,62 @@ def restaurant_finder(filled_slots, restaurant_info):
     return possible_restaurants
 
 #gets if final restaurant or not (enkelvoud of meervoud zin teruggeven)
-def template_generator(filled_slots, restaurant_info, speech_act, current_restaurant):
+def template_generator(filled_slots, speech_act, current_suggested_restaurant):
 
     #ack,affirm,confirm,deny,inform,negate,null,repeat,reqalts,reqmore,request,restart
 
     if speech_act == "hello":
         return "Welcome to the team14 restaurant system. You can tell your preference of area , price range or food type. How can we help?"
 
-    # if no preference / slots yet but the speech act is not inform
+    # if no preference / slots yet but the speech act is not inform, force them to give preference
     elif (not filled_slots) and (speech_act != 'inform'):
         # ask for restaurant preference
         return "What kind of food would you like?"
 
-    elif speech_act == "bye" or "thankyou":
-        return "Thank you for using team14 system. Bye."
+    elif (speech_act == "bye") or (speech_act == "thankyou"):
+        return "Thank you for using team14 system. eet smakelijk!"
 
     elif speech_act == "request":
         #TODO which info is asked by user?
-        return restaurant_finder(filled_slots, restaurant_info)
+        return current_suggested_restaurant
 
     elif speech_act == "inform":
-        if len(restaurant_finder(filled_slots, restaurant_info)) == 1:
-            restaurant = restaurant_finder(filled_slots, restaurant_info)
-            return str(restaurant[0]) + "is a nice place"
+        # if no restaurant exists with a complete slots: give back to user and ask other preferences
+        if len(current_suggested_restaurant) == 0:
+            return "We cannot find any restaurant. What other kind of restaurant you like?"
 
+        # if 1 such restaurant exists: return restaurant
+        elif len(current_suggested_restaurant) == 1:
+            name = str(current_suggested_restaurant[0][0])
+            pricerange = str(current_suggested_restaurant[0][1])
+            area = str(current_suggested_restaurant[0][2])
+            food = str(current_suggested_restaurant[0][3])
+
+            if area != '':
+                return '{} is a {} {} restaurant in the are of {}'.format(name, pricerange, food, area)
+            else:
+                return '{} is a {} {} restaurant'.format(name, pricerange, food)
+
+        # if more: return the number of restaurants and ask for missing slot info
+        elif len(current_suggested_restaurant) > 1:
+            response = 'There are {} restaurants found. '.format(len(current_suggested_restaurant))
+
+            try:
+                filled_slots['pricerange']
+            except KeyError:
+                response += 'What is your price range? '
+
+            try:
+                filled_slots['area']
+            except KeyError:
+                response += 'Where is your preference area? '
+
+            try:
+                filled_slots['food']
+            except KeyError:
+                response += 'What type of food do you like? '
+
+            return response
 
 if __name__ == "__main__":
     #need to be filled in volgorde van de user als er geen slot is niks meegeven
