f51f528f45ec584983465b29a4b515d3242d4ee7
jonivde <jonivde@hotmail.com>
2018-10-28 13:27:44 +0100

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,6 +1,8 @@
 from template_generator import *
+from LSTM import model_user
 
 user_sentences = []
+system_sentences = []
 current_suggested_restaurant = []
 filled_slots = {}
 
@@ -9,14 +11,17 @@ def dialogue_ender():
     global user_sentences
     global current_suggested_restaurant
     global filled_slots
+    global system_sentences
     filled_slots = {}
     user_sentences = []
     current_suggested_restaurant = []
+    system_sentences = []
 
 def manager():
     global user_sentences
     global current_suggested_restaurant
     global filled_slots
+    global system_sentences
     user_response = ''
     restaurant_info = csv_reader()
     is_welcome = True
@@ -27,11 +32,13 @@ def manager():
         # to check if this is just welcome
         if is_welcome:
             speech_act = 'hello'
-        # otherwise, check what is the speech act of the user response
+        # otherwise, check what is the speech act of the user response (joni: speech act is always null?)
         else:
-            speech_act = speech_act_finder(user_response)
+            print(user_response, "user_response")
+            speech_act = model_user(user_response)
+            print(speech_act, "is the classified speech act")
 
-        # STORE SPEECH ACT AND SENTENCE
+        # STORE SPEECH ACT AND SENTENCE (joni: this works)
         user_sentence = {}
         user_sentence['sentence'] = user_response
         user_sentence['speech_act'] = speech_act
@@ -46,7 +53,7 @@ def manager():
             or speech_act == "ack" \
             or speech_act == "null" \
             or speech_act == "reqalts":
-            #TODO get real slots value
+            #TODO get real slots value (joni: don't know which part of the parser this is?
             get_filled_slots = {
                 "pricerange": "expensive",
                 "area": "north",
@@ -58,13 +65,19 @@ def manager():
             #GET POSSIBLE RESTAURANT
             current_suggested_restaurant = restaurant_finder(filled_slots, restaurant_info)
 
-        # GET TEMPLATE
-        template_result = template_generator(filled_slots, speech_act, current_suggested_restaurant)
+        # if user wants us to repeat last sentence do that otherwise, find template
+        if speech_act == 'repeat':
+            template_result = system_sentences[-1]
+        else:
+            template_result = template_generator(filled_slots, speech_act, current_suggested_restaurant)
+        #store system sentences
+        system_sentences.append(template_result)
 
         print('System: '+template_result)
 
-        # if no restaurant found, restart the dialogue
-        if template_result != "We cannot find any restaurant. What other kind of restaurant you like?":
+        # if no restaurant found or user wants to reset, restart the dialogue
+        if template_result == "We cannot find any restaurant. What other kind of restaurant would you like?"\
+            or speech_act == 'deny':
             dialogue_ender()
 
         # as long as the template result is not the end of dialogue, then keep expecting user input
@@ -75,11 +88,7 @@ def manager():
             # END DIALOGUE
             break
 
-        is_welcome = False;
-
-#def sentence_checker(sentence):
-    #store user sentence
-    #determine speech act
+        is_welcome = False
 
 if __name__ == "__main__":
     manager()
