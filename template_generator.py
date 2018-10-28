7d173be1d5302433404e627d16b56078467fa792
al <a.k.sugiharto@gmail.com>
2018-10-28 06:04:50 +0100

    more templates

--- template_generator.py
+++ template_generator.py
@@ -54,63 +54,91 @@ def restaurant_finder(filled_slots, restaurant_info):
             possible_restaurants.append(restaurant)
     return possible_restaurants
 
+def template_restart():
+    return "What kind of food would you like?"
+
+def template_bye():
+    return "Thank you for using team14 system. eet smakelijk!"
+
+def template_hello():
+    return "Welcome to the team14 restaurant system. You can tell your preference of area , price range or food type. How can we help?"
+
+def template_inform_multiple_results(filled_slots, current_suggested_restaurant):
+    response = 'There are {} restaurants found. '.format(len(current_suggested_restaurant))
+
+    try:
+        filled_slots['pricerange']
+    except KeyError:
+        response += 'What is your price range? '
+
+    try:
+        filled_slots['area']
+    except KeyError:
+        response += 'Where is your preference area? '
+
+    try:
+        filled_slots['food']
+    except KeyError:
+        response += 'What type of food do you like? '
+
+    return response
+
+def template_import_the_one(current_suggested_restaurant):
+    name = str(current_suggested_restaurant[0][0])
+    pricerange = str(current_suggested_restaurant[0][1])
+    area = str(current_suggested_restaurant[0][2])
+    food = str(current_suggested_restaurant[0][3])
+
+    if area != '':
+        return '{} is a {} {} restaurant in the are of {}'.format(name, pricerange, food, area)
+    else:
+        return '{} is a {} {} restaurant'.format(name, pricerange, food)
+
+def template_no_restaurant_found():
+    return "We cannot find any restaurant. What other kind of restaurant you like?"
+
 #gets if final restaurant or not (enkelvoud of meervoud zin teruggeven)
 def template_generator(filled_slots, speech_act, current_suggested_restaurant):
 
-    #ack,affirm,confirm,deny,inform,negate,null,repeat,reqalts,reqmore,request,restart
+    #ack,affirm,confirm,null,repeat,reqalts
 
     if speech_act == "hello":
-        return "Welcome to the team14 restaurant system. You can tell your preference of area , price range or food type. How can we help?"
+        return template_hello()
 
     # if no preference / slots yet but the speech act is not inform, force them to give preference
     elif (not filled_slots) and (speech_act != 'inform'):
-        # ask for restaurant preference
-        return "What kind of food would you like?"
+        return template_restart()
 
-    elif (speech_act == "bye") or (speech_act == "thankyou"):
-        return "Thank you for using team14 system. eet smakelijk!"
+    elif speech_act == 'restart':
+        return template_restart()
+
+    elif speech_act == 'deny':
+        return template_restart()
+
+    elif (speech_act == "bye"):
+        return template_bye()
+
+    elif (speech_act == "thankyou"):
+        return template_bye
 
     elif speech_act == "request":
         #TODO which info is asked by user?
         return current_suggested_restaurant
 
-    elif speech_act == "inform":
+    #negate is similar like inform, only usually started with "no"
+    #reqmoe system can repeat the same answer for inform
+    elif speech_act == "inform" or speech_act == "reqmore" or speech_act == "negate":
         # if no restaurant exists with a complete slots: give back to user and ask other preferences
         if len(current_suggested_restaurant) == 0:
-            return "We cannot find any restaurant. What other kind of restaurant you like?"
+            return template_no_restaurant_found()
 
         # if 1 such restaurant exists: return restaurant
         elif len(current_suggested_restaurant) == 1:
-            name = str(current_suggested_restaurant[0][0])
-            pricerange = str(current_suggested_restaurant[0][1])
-            area = str(current_suggested_restaurant[0][2])
-            food = str(current_suggested_restaurant[0][3])
-
-            if area != '':
-                return '{} is a {} {} restaurant in the are of {}'.format(name, pricerange, food, area)
-            else:
-                return '{} is a {} {} restaurant'.format(name, pricerange, food)
+            return template_import_the_one(current_suggested_restaurant)
 
         # if more: return the number of restaurants and ask for missing slot info
         elif len(current_suggested_restaurant) > 1:
-            response = 'There are {} restaurants found. '.format(len(current_suggested_restaurant))
-
-            try:
-                filled_slots['pricerange']
-            except KeyError:
-                response += 'What is your price range? '
-
-            try:
-                filled_slots['area']
-            except KeyError:
-                response += 'Where is your preference area? '
-
-            try:
-                filled_slots['food']
-            except KeyError:
-                response += 'What type of food do you like? '
-
-            return response
+            return template_inform_multiple_results(filled_slots, current_suggested_restaurant)
 
 if __name__ == "__main__":
     #need to be filled in volgorde van de user als er geen slot is niks meegeven
