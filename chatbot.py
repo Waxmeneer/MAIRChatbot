c115d893fc589480079773ee12a05da77d29eb10
mauwie1 <m.r.politiek@students.uu.nl>
2018-10-30 20:22:32 +0100

    Bugfixing etc, should work now

--- chatbot.py
+++ chatbot.py
@@ -1,4 +1,4 @@
-import sys
+import sys, csv
 from template_generator import *
 from LSTM import model_user, model_trainer, load_tokenizer, load_model, load_speechactdict
 
@@ -9,13 +9,25 @@ dialogue = []
 suggested_restaurants = []
 filled_slots = {}
 
+# makes a list with lists of possible restaurants
+# typical element in restaurant list is [name, pricerange, area, food, phone, addr, postcode]
+def csv_reader():
+    with open('restaurantinfo.csv') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        restaurant_info = []
+        for row in csv_reader:
+            restaurant_info.append(list(row))
+        restaurant_info.pop(0)
+    return restaurant_info
+
+
 
 def dialogue_ender():
     # clear all global variables
     global suggested_restaurants, filled_slots, asked_slots
     filled_slots = {}
     dialogue.clear()
-    current_suggested_restaurant.clear()
+    suggested_restaurants.clear()
     asked_slots.clear()
 
 
@@ -25,75 +37,59 @@ def manager():
     model, tokenizer, speechactdict = load_model(), load_tokenizer(), load_speechactdict()
     restaurant_info = csv_reader()
     is_welcome = True
-    order=0
+    order = 0
     # start dialogue
-    print("Welcome to the Chatbot system: ")
-    print("What kind of restaurant are you looking for? You can ask for example for food type, area or pricerange.")
+    print(template_hello())
     while True:
         # get speech act of the user input.
         inp = input("user: ")
         speech_act = model_user(inp, model, tokenizer, speechactdict)
         slots = slot_dict(inp)
-        orderinfo=update_order(order, slots)
+        orderinfo = update_order(order, slots)
         order, slots = orderinfo[0], orderinfo[1]
         filled_slots = slot_change(filled_slots, slots, speech_act, dialogue)
         dialogue.append([speech_act, inp])
-        
-        print(speech_act, "is the classified speech act")
-        """
+
         # if user wants us to repeat last sentence do that otherwise, find template
-        if speech_act == 'repeat':
-            template_result = system_sentences[-1]
-        
-        #Get asked slots of previous system utterance 
-        elif speech_act == 'inform' and len(suggested_restaurants) > 1:
-            result = template_generator(filled_slots, speech_act, suggested_restaurants, dialogue)
-            template_result = result[0]
-            asked_slots = result[1]
-            print(asked_slots)
-        else:"""
-        template_result = template_generator(filled_slots, speech_act, suggested_restaurants, dialogue)
-        template_str = template_result[0]
-        template_sug = template_result[1]
+        #if speech_act == 'repeat':
+         #   template_result = system_sentences[-1]
+
+        template_result = template_generator(filled_slots, slots, suggested_restaurants, restaurant_info, dialogue)
+        try: #Suggested restaurants only changes on inform.
+            template_str = template_result[0]
+            template_sug = template_result[1]
+            suggested_restaurants = template_sug
+        except:
+            template_str = template_result
         # store system sentences
-        dialogue.append([99, template_str]) #99 is an arbitrary speech act.
-
+        dialogue.append([99, template_str])  # 99 is an arbitrary speech act.
+        print('')
         print('System: ' + template_str)
-
         # if no restaurant found or user wants to reset, restart the dialogue
         if template_str == "We cannot find any restaurant. What other kind of restaurant would you like?" \
                 or speech_act == 'deny':
             dialogue_ender()
 
-        # as long as the template result is not the end of dialogue, then keep expecting user input
-        if template_str != 'Thank you for using team14 system. Bye.':
-            user_response = input('User: ')
-        # ending the dialog by saying bye and exiting the system
-        else:
-            # END DIALOGUE
-            break
 
-        is_welcome = False
-        
 def slot_change(filled_slots, slots, speech_act, dialogue):
     if speech_act == 'inform':
         for key, val in slots.items():
-            print(slots)
-            filled_slots[key]=val
+            filled_slots[key] = val
     elif speech_act == 'request':
         pass
     elif speech_act == 'negate':
         pass
     return filled_slots
 
+
 def update_order(order, slot_dict):
-    count=0
+    count = 0
     for item in slot_dict.values():
-        for slot in item:
-            slot[1]+=order
-            count+=1
-    order+=count
-    return[order, slot_dict]
-        
+        item[0] += order #Every first element is the order
+        count += 1
+    order += count
+    return [order, slot_dict]
+
+
 if __name__ == "__main__":
     manager()
--- template_generator.py
+++ template_generator.py
@@ -1,20 +1,11 @@
-import csv
+
 from LSTM import model_trainer, model_user
-#Keep track of suggested restaurant if user wants another restaurant
-#Make templates and keep track of order in which user states preferences
-#aggregation?
 
 
-#makes a list with lists of possible restaurants
-#typical element in restaurant list is [name, pricerange, area, food, phone, addr, postcode]
-def csv_reader():
-    with open('restaurantinfo.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        restaurant_info = []
-        for row in csv_reader:
-            restaurant_info.append(list(row))
-        restaurant_info.pop(0)
-    return restaurant_info
+# Keep track of suggested restaurant if user wants another restaurant
+# Make templates and keep track of order in which user states preferences
+# aggregation?
+
 
 def template_request(restaurant, sentence):
     possible_requests = {"phone": ["phone"],
@@ -23,7 +14,7 @@ def template_request(restaurant, sentence):
                          "price": ["price"],
                          "food": ["type"],
                          "area": ["area"]
-    }
+                         }
     restaurant = restaurant[0]
     info = ''
     for key, values in possible_requests.items():
@@ -44,95 +35,131 @@ def template_request(restaurant, sentence):
     elif info == "food":
         return "The food type of {} is {} ".format(str(restaurant[0]), str(restaurant[3]))
     else:
-        return "Sorry, what information would you like to know about {}? Phone number, address, postcode, price range, area or type of food?".format(str(restaurant[0]))
+        return "Sorry, what information would you like to know about {}? Phone number, address, postcode, price range, area or type of food?".format(
+            str(restaurant[0]))
+
 
 def restaurant_finder(filled_slots, restaurant_info, suggested_restaurants):
     possible_restaurants = []
     values_filled_slots = []
 
-    #make list of slot values which are not None
+    # make list of slot values which are not None
     for key, value_list in filled_slots.items():
-            value = value_list[0][0]
+        slot = value_list[1:]
+        for value in slot:
             if value == 'random':
                 values_filled_slots.append(key)
-            values_filled_slots.append(str(value))
-    print(values_filled_slots, "these are the value filled slots")
-    #make list of restaurant values and compare
+            else:
+                values_filled_slots.append(value)
+    # make list of restaurant values and compare
     for restaurant in restaurant_info:
         info_elements = restaurant[1:4]
-        if set(values_filled_slots).issubset(info_elements) and restaurant not in suggested_restaurants:
-            possible_restaurants.append(restaurant)
+        if set(values_filled_slots).issubset(info_elements):
+            if restaurant not in suggested_restaurants:
+                possible_restaurants.append(restaurant)
     return possible_restaurants
+
 
 def template_restart():
     return "What kind of restaurant would you like?"
 
+
 def template_bye():
     return "Thank you for using team14 system. eet smakelijk!"
+
 
 def template_hello():
     return "Welcome to the team14 restaurant system. You can tell your preference of area , price range or food type. How can we help?"
 
-def template_inform_multiple_results(filled_slots, current_suggested_restaurant):
-    response = 'There are {} restaurants found. '.format(len(current_suggested_restaurant))
-    asked_slots = []
-    try:
-        filled_slots['pricerange']
-    except KeyError:
-        response += 'What is your price range? '
-        asked_slots.append('pricerange')
+def template_inform(filled_slots, slot_dict, poss_rests, suggested_restaurants, restaurant_info):
+    for key, val in slot_dict.items():
+        filled_slots[key] = val
+    informinfo = inform_to_string(filled_slots, poss_rests, suggested_restaurants, restaurant_info)
+    return [informinfo[0],
+            informinfo[1]]  # Need to also return suggested_restaurants, as a new restaurant may have been suggested.
 
-    try:
-        filled_slots['area']
-    except KeyError:
-        response += 'What is your area of preference? '
-        asked_slots.append('area')
 
-    try:
-        filled_slots['food']
-    except KeyError:
-        response += 'What type of food would you like? '
-        asked_slots.append('food')
+# Puts all inform speech act info into a response string, combined with the updated already suggested restaurants.
+def inform_to_string(filled_slots, poss_rests, suggested_restaurants, restaurant_info):
+    if len(filled_slots.keys()) > 2 and len(poss_rests) > 0 or len(poss_rests)==1:
+        suggested_restaurants.append(poss_rests[0])
+        return ['Restaurant: \"' + poss_rests[0][0] + '\" is a nice restaurant ' + slots_to_string(filled_slots),
+            suggested_restaurants]
+    elif len(filled_slots.keys()) < 3 and len(poss_rests) > 0:
+        missing_slot = get_missing_slot(filled_slots)
+        return ['There are ' + str(len(poss_rests)) + ' restaurants found ' + slots_to_string(
+            filled_slots) + 'What ' + missing_slot + ' would you like?', suggested_restaurants]
 
-    return [response, asked_slots]
+    else:  # When the amount of possible restaurants is 0
+        if len(restaurant_finder(filled_slots, restaurant_info, []))>0: #If there are restaurants, but the user rejected all.
+            return ['Sorry but there are no other restaurants ' + slots_to_string(filled_slots) + '\n' +
+                    'Could you please change the food, area or pricerange?', suggested_restaurants]
+        else:
+            return ['Sorry but there are no restaurants ' + slots_to_string(filled_slots) + '\n' +
+                    'Could you please change the food, area or pricerange?', suggested_restaurants]
 
-def template_import_the_one(current_suggested_restaurant):
-    current_suggested_restaurant = current_suggested_restaurant[0]
-    name = str(current_suggested_restaurant[0])
-    pricerange = str(current_suggested_restaurant[1])
-    area = str(current_suggested_restaurant[2])
-    food = str(current_suggested_restaurant[3])
 
-    if area != '':
-        return '{} is a {} {} restaurant in the {} area of town '.format(name, pricerange, food, area)
-    else:
-        return '{} is a {} {} restaurant'.format(name, pricerange, food)
+# Gets a slot that is not yet filled.
+def get_missing_slot(filled_slots):
+    possible_slots = ['food', 'area', 'pricerange']
+    for slot in possible_slots:
+        if slot not in filled_slots.keys():
+            return slot
+
+
+# Takes the filled slots, orders it by their utterance order, and transforms them to a string to be uttered by the system.
+def slots_to_string(filled_slots):
+    orderedlist = []
+    string = ''
+    for key, value in filled_slots.items():
+        slots = value[1:]
+        order = value[0]
+        if key == 'area':
+            orderedlist.append([order, 'in the ' + ",".join(slots) + ' part of town'])
+        elif key == 'food':
+            orderedlist.append([order, 'serving ' + ",".join(slots) + ' food'])
+        elif key == 'pricerange':
+            orderedlist.append([order, 'with a ' + ",".join(slots) + ' priced menu'])
+            orderedlist.sort(key=lambda x: x[0])
+    for slot in orderedlist:
+        string += slot[1]
+        try: #If in some case the orderlist is empty, this catches it.
+            if slot == orderedlist[-1]: #The last element is the end of the sentence
+                string+='. '
+            else:
+                string+=' '
+        except:
+            pass
+    return string
+
 
 def template_no_restaurant_found():
     return "We cannot find any restaurant. What other kind of restaurant you like?"
 
-#gets if final restaurant or not (enkelvoud of meervoud zin teruggeven)
-def template_generator(filled_slots, slot_dict, suggested_restaurants, dialogue):
+
+# gets if final restaurant or not (enkelvoud of meervoud zin teruggeven)
+def template_generator(filled_slots, slot_dict, suggested_restaurants, restaurant_info, dialogue):
     poss_rests = restaurant_finder(filled_slots, restaurant_info, suggested_restaurants)
-    speech_act = dialogue[len(dialogue)-1][0]
-    current_user_sentence = dialogue[len(dialogue)-1][1] #At the end of the dialogue list
-    current_suggested_restaurant = suggested_restaurants[len(suggested_restaurants)-1]
-    
+    speech_act = dialogue[len(dialogue) - 1][0]
+    current_user_sentence = dialogue[len(dialogue) - 1][1]  # At the end of the dialogue list
+
+
+    current_suggested_restaurant = suggested_restaurants[-1:] #last element of list, if it's empty then returns empty list
     if speech_act == "hello":
         return template_hello()
 
     # if no preference / slots yet but the speech act is not inform, force them to give preference
-    elif (not filled_slots) and (speech_act != 'inform') or speech_act=='restart':
+    elif ((not filled_slots) and (speech_act != 'inform')) or speech_act == 'restart':
         return template_restart()
-    
-#joni:not restart, but search for another restaurant I guess while holding on to filled slots...
+
+    # joni:not restart, but search for another restaurant I guess while holding on to filled slots...
     elif speech_act == 'deny':
         return template_restart()
 
-    elif (speech_act == "bye"):
+    elif speech_act == "bye":
         return template_bye()
 
-    elif (speech_act == "thankyou"):
+    elif speech_act == "thankyou":
         return template_bye()
 
     elif speech_act == "request":
@@ -148,36 +175,27 @@ def template_generator(filled_slots, slot_dict, suggested_restaurants, dialogue)
         elif len(current_suggested_restaurant) > 1:
             return template_inform_multiple_results(filled_slots, current_suggested_restaurant)
 
-    #negate is similar like inform, only usually started with "no"
-    #reqmoe we can repeat the same answer for inform, only confirming
-    #confirm  we can repeat the same answer for inform, only confirming
-    #affirm this is also another way of confirming
-    #ack use a lot words like "um" or "okay", another way of confirming or sometimes adding more info like "hmm how about korean?"
-    #null super random, but when we return what we have in inform, it sounds still related
-    #reqalts is very similar like inform
+    # negate is similar like inform, only usually started with "no"
+    # reqmoe we can repeat the same answer for inform, only confirming
+    # confirm  we can repeat the same answer for inform, only confirming
+    # affirm this is also another way of confirming
+    # ack use a lot words like "um" or "okay", another way of confirming or sometimes adding more info like "hmm how about korean?"
+    # null super random, but when we return what we have in inform, it sounds still related
+    # reqalts is very similar like inform
 
     elif speech_act == "inform" \
             or speech_act == "reqmore" \
             or speech_act == "negate" \
             or speech_act == "confirm" \
-            or speech_act == "affirm"\
-            or speech_act == "ack"\
+            or speech_act == "affirm" \
+            or speech_act == "ack" \
             or speech_act == "null" \
             or speech_act == "reqalts":
         # if no restaurant exists with a complete slots: give back to user and ask other preferences
-        if len(current_suggested_restaurant) == 0:
-            return template_no_restaurant_found()
-
-        # if 1 such restaurant exists: return restaurant
-        elif len(current_suggested_restaurant) == 1:
-            return template_import_the_one(current_suggested_restaurant)
-
-        # if more: return the number of restaurants and ask for missing slot info
-        elif len(current_suggested_restaurant) > 1:
-            return template_inform_multiple_results(filled_slots, current_suggested_restaurant)
+        return(template_inform(filled_slots, slot_dict, poss_rests, suggested_restaurants, restaurant_info))
 
 if __name__ == "__main__":
-    #need to be filled in volgorde van de user als er geen slot is niks meegeven
+    # need to be filled in volgorde van de user als er geen slot is niks meegeven
     filled_slots = {
         "pricerange": "expensive",
         "area": "north",
@@ -185,5 +203,5 @@ if __name__ == "__main__":
     }
     restaurant_info = csv_reader()
     print(restaurant_finder(filled_slots, restaurant_info))
-    #analyze speech act for every sentence
-    #keep track of current restaurant
+    # analyze speech act for every sentence
+    # keep track of current restaurant
